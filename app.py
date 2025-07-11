e87e80b56d4bf5b9a7fdc20772d74ef7312a307a
Janhavi Dubey <janhavi.dubey2025@gmail.com>
2025-07-11 14:59:40 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request
 from auth import auth_bp
-from database import get_feedbacks
+from database import get_feedbacks, save_feedback
 import os
 
 app = Flask(__name__)
@@ -16,13 +16,23 @@ def home():
 def dashboard():
     if 'username' not in session:
         return redirect(url_for('auth.login'))
-    
+
     username = session['username']
     if username.endswith("admin"):
         feedbacks = get_feedbacks()
         return render_template('admin_dashboard.html', username=username, feedbacks=feedbacks)
     else:
         return render_template('user_dashboard.html', username=username)
+
+@app.route('/submit-feedback', methods=['POST'])
+def submit_feedback():
+    if 'username' not in session:
+        return redirect(url_for('auth.login'))
+
+    user = session['username']
+    content = request.form['content']
+    save_feedback(user, content)
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.run(debug=True)
